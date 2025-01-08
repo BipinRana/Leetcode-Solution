99cf7d5992b8fa79e515c3d66b8e4f3dfdebfbd3
Bipin Rana <106875746+BipinRana@users.noreply.github.com>
2025-01-08 23:19:18 +0545

    Create Binary Tree Postorder Traversal.py

---
+++ Binary Tree Postorder Traversal.py
@@ -0,0 +1,32 @@
+# Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+# Example 1:
+# Input: root = [1,null,2,3]
+# Output: [3,2,1]
+
+# Example 2:
+# Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+# Output: [4,6,7,5,2,9,8,3,1]
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        def result(root):
+            if root is None:
+                return []
+            left = result(root.left)
+            right = result(root.right)
+            return left + right + [root.val]
+        return result(root)
+
+
+        
