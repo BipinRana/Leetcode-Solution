a711974f238eff935d4c948a118c9e86722c8d8d
Bipin Rana <106875746+BipinRana@users.noreply.github.com>
2025-07-26 17:56:55 +0545

    Create LongestValidParenthesis.py

---
+++ LongestValidParenthesis.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+        stack = [-1]
+        max_score = 0
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_score = max(max_score,i-stack[-1])
+        return max_score
+        
